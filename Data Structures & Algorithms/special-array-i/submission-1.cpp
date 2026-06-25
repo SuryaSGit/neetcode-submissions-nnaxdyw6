fd362e3eb48552e52e1104796ceddcad0bf05aeb
Surya Sridharan <88287942+SuryaSGit@users.noreply.github.com>
2026-06-25 23:17:08 +0530

    Add: special-array-i - submission-1

---
+++ Data Structures & Algorithms/special-array-i/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        if(nums.size() == 1){
+            return true;
+        }
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i]%2 == 0){
+                if(i > 0 && nums[i-1]%2 == 0){
+                    return false;
+                }
+                if(i < nums.size() - 1 && nums[i + 1]%2 == 0){
+                    return false;
+                }
+            }
+            if(nums[i]%2 == 1){
+                if(i > 0 && nums[i-1]%2 == 1){
+                    return false;
+                }
+                if(i < nums.size() - 1 && nums[i + 1]%2 == 1){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
