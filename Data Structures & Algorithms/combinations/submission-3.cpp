c8e02dc40e8764ff2c31fa9df1cb81194558ff21
Surya Sridharan <88287942+SuryaSGit@users.noreply.github.com>
2026-08-12 11:02:07 +0530

    Add: combinations - submission-3

---
+++ Data Structures & Algorithms/combinations/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>>& res, vector<int>& cur, int cur_pos, int n, int k){
+        if(cur.size() == k){
+            res.push_back(cur);
+            return;
+        }
+        if(cur_pos > n){
+            return;
+        }
+        cur.push_back(cur_pos);
+        dfs(res,cur,cur_pos+1,n,k);
+        cur.pop_back();
+        dfs(res,cur,cur_pos+1,n,k);
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> res;
+        vector<int> temp;
+        dfs(res,temp,1,n,k);
+        return res;
+    }
+};
